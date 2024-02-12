4f785f8adbf2425e66bb9a5a4833d7462fbcbee5
Minsung_Kang <kminsung030120@gmail.com>
2024-02-12 22:59:40 +0900

    2024-02-12 평범한 배낭

---
+++ rivkms/DP/12865.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+    int n, k;
+
+    cin >> n >> k;
+    vector<pair<int, int>> wv(n+z1, pair<int, int>(0,0));
+    vector<vector<int>> value (n+1, vector<int>(k+1, 0));
+
+    for(int i = 1; i<=n; i++){
+        cin >> wv[i].first >> wv[i].second;        
+    }
+    
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=k;j++){
+            if(wv[i].first > j ){
+                value[i][j] = value[i-1][j];
+            }
+            else{
+                value[i][j] = max(value[i-1][j], value[i-1][j-wv[i].first]+wv[i].second);
+            }
+            
+        }
+    }
+    cout << value[n][k];
+
+    return 0;
+}
