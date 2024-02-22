779de321beffd1ace9fb7617e809e1ee7133badd
mong3125 <mong3125@naver.com>
2024-02-22 20:19:02 +0900

    백준 11724번 연결 요소의 개수 java solution

--- mong3125/DFS/P023_11724.java
+++ mong3125/DFS/P023_11724.java
@@ -3,14 +3,12 @@ package CH05_탐색;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class P023_11724 {
     static int N, M;
-    static int[][] edges;
-    static int[] cache;
+    static boolean[][] edges;
+    static boolean[] cache;
 
     public static void main(String[] args) throws IOException {
         // 입력
@@ -21,8 +19,8 @@ public class P023_11724 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        edges = new int[N + 1][N + 1];
-        cache = new int[N + 1];
+        edges = new boolean[N + 1][N + 1];
+        cache = new boolean[N + 1];
 
         // 엣지 입력
         for (int i = 0; i < M; i++) {
@@ -31,13 +29,13 @@ public class P023_11724 {
             int b = Integer.parseInt(st.nextToken());
 
             // 양방향 입력
-            edges[a][b] = 1;
-            edges[b][a] = 1;
+            edges[a][b] = true;
+            edges[b][a] = true;
         }
 
         int count = 0;
         for (int i = 1; i <= N; i++) {
-            if (dfs(i, i)) {
+            if (dfs(i)) {
                 count++;
             }
         }
@@ -47,14 +45,14 @@ public class P023_11724 {
         br.close();
     }
 
-    public static boolean dfs(int id, int check) {
-        if (cache[id] != 0) return false;
+    public static boolean dfs(int id) {
+        if (cache[id]) return false;
 
-        cache[id] = check;
+        cache[id] = true;
 
         for (int i = 1; i <= N; i++) {
-            if (edges[id][i] == 1) {
-                dfs(i, check);
+            if (edges[id][i]) {
+                dfs(i);
             }
         }
 
