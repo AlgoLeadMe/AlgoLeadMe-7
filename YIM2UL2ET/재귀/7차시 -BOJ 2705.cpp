5f4b652426dfa4527bf59f023738320f81dedbc1
임승호 <132066506+YIM2UL2ET@users.noreply.github.com>
2024-03-02 03:15:02 +0900

    코드 수정

--- YIM2UL2ET/재귀/7차시 -BOJ 2705.cpp
+++ YIM2UL2ET/재귀/7차시 -BOJ 2705.cpp
@@ -4,15 +4,16 @@ int ary[1000] = {0};
 
 int solve(int n)
 {   
-    if (n == 1) return 1;
-    else if (ary[n-1] != 0) return ary[n-1];
-    for (int i = 0; n-i*2 > 1; i++) ary[n-1] += solve(i+1);
+    if (ary[n-1] != 0) return ary[n-1];
+    for (int i = 0; n-i*2 > 1;) ary[n-1] += solve(++i);
     return ++ary[n-1];
 }
 
 int main(void)
 {
     int n, input;
+    ary[0] = 1;
+
     std::cin >> n;
     for (int i = 0; i < n; i++) {
         std::cin >> input;
