08e3f4701542aa1abdec5f6e2d1f3f3834b6b102
mong3125 <mong3125@naver.com>
2024-03-21 02:04:18 +0900

    BOJ1920_수찾기 java solution

--- mong3125/이분탐색/BOJ1920_수찾기.java
+++ mong3125/이분탐색/BOJ1920_수찾기.java
@@ -1,12 +1,10 @@
-package 이진탐색;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class BOJ1920_수찾기 {
+public class Main {
 
     static int[] A;
     static int[] targets;
